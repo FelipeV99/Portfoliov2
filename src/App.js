a6e112051ac32a4e5f21b5a74f39e3809960da4e
unknown <afelipea99@gmail.com>
2024-04-27 07:46:53 -0500

    adding hotjar trying 2

--- src/App.js
+++ src/App.js
@@ -23,19 +23,10 @@ function App() {
   let gothicLoaded = false;
 
 
-  (function (h, o, t, j, a, r) {
-    h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
-    h._hjSettings = { hjid: 4961378, hjsv: 6 };
-    a = o.getElementsByTagName('head')[0];
-    r = o.createElement('script'); r.async = 1;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-    a.appendChild(r);
-  })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-
 
   useEffect(() => {
     hotjar.initialize(4961378, 6);
-  }, []);
+    }, []);
 
 
   const appRef = useRef();
