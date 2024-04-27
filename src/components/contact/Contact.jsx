c4e7b9a336785884774b813f790bd82c2dee6b30
unknown <afelipea99@gmail.com>
2024-04-26 23:54:51 -0500

    added external icons to responsive mobile

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
         });
         setTimeout(() => {
             ScrollTrigger.refresh();
-            
+
         }, 2000);
     });
     useEffect(() => {
@@ -50,7 +50,7 @@ const Contact = () => {
 
                 <div id="title-cta" >
                     <div className='socials-asterisk'>
-                    <img src={require("../../assets/star-nav.svg").default} alt="" />
+                        <img src={require("../../assets/star-nav.svg").default} alt="" />
                         <p className='p2 off-black-800'>{t("socials")}</p>
 
                     </div>
@@ -70,8 +70,15 @@ const Contact = () => {
                         {/* <div className='pill'>
                             <p className='p2 letter-s1'>{t("socials")}</p>
                         </div> */}
-                        <div className='social-row'>
-                            <p className='bolden p2 off-black-800'>mail</p>
+                         <div className='social-row'>
+                            <div className='top-social-row'>
+                                <p className='bolden p2 off-black-800'>mail</p>
+                                <a href="mailto:afelipea99@gmail.com">
+                                    <img className="social-icon-mobile" src={require("../../assets/external-arrow.svg").default} alt="" />
+                                </a>
+
+                            </div>
+
                             <div className='link-icon'>
                                 <p className='p2 off-black-800'>afelipea99@gmail.com</p>
                                 <a href="mailto:afelipea99@gmail.com">
@@ -80,14 +87,22 @@ const Contact = () => {
                             </div>
                         </div>
                         <div className='social-row' >
-                            <p className='bolden p2 off-black-800'>behance</p>
+                            <div className='top-social-row'>
+                                <p className='bolden p2 off-black-800'>behance</p>
+                                <img className="social-icon-mobile" src={require("../../assets/external-arrow.svg").default} onClick={() => { window.open("https://www.behance.net/felipeandrade27") }} alt="" />
+
+                            </div>
                             <div className='link-icon'>
                                 <p className='p2 off-black-800'>www.behance.net/felipeandrade27</p>
                                 <img className="social-icon" src={require("../../assets/external-arrow.svg").default} onClick={() => { window.open("https://www.behance.net/felipeandrade27") }} alt="" />
                             </div>
                         </div>
                         <div className='social-row'>
-                            <p className='bolden p2 off-black-800'>linkedin</p>
+                            <div className='top-social-row'>
+                                <p className='bolden p2 off-black-800'>linkedin</p>
+                                <img className="social-icon-mobile" src={require("../../assets/external-arrow.svg").default} alt="" onClick={() => { window.open("https://www.linkedin.com/in/felipe-a-411294138/") }} />
+
+                            </div>
                             <div className='link-icon'>
                                 <p className='p2 off-black-800'>www.linkedin.com/in/felipe-a-411294138/</p>
                                 <img className="social-icon" src={require("../../assets/external-arrow.svg").default} alt="" onClick={() => { window.open("https://www.linkedin.com/in/felipe-a-411294138/") }} />
