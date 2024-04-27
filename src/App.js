5c8fced9396aaaeca8738a47bd16fc064ed8651d
unknown <afelipea99@gmail.com>
2024-04-27 08:43:09 -0500

    adding hotjar trying 6

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ import ScrollToTop from './components/scrollToTop/ScrollToTop';
 import { FontFaceObserver } from "font-face-observer";
 import Lottie from 'react-lottie';
 import PreloaderAnimation from "../src/assets/PreloaderAnimation.json";
-import { hotjar } from 'react-hotjar';
+import Hotjar from './components/hotjar/Hotjar';
 
 export const AppTagContext = React.createContext();
 
 function App() {
-  hotjar.initialize(4961378, 6);
+
+
 
   const FontFaceObserver = require('fontfaceobserver');
   const font = new FontFaceObserver('IDGSemi');
@@ -62,12 +63,15 @@ function App() {
   return (
     <>
       {fontLoaded ?
+
         <AppTagContext.Provider value={appRef}>
+          <Hotjar />
           <div className="App" ref={appRef}>
             <Router>
               <Navbar />
               <ScrollToTop />
               <Routes>
+
                 <Route path="/" element={<Home />} />
                 <Route path="/case-study/olabNew" element={<CaseStudyNew />} />
 
--- src/components/caseStudyNew/CaseStudyNew.jsx
+++ src/components/caseStudyNew/CaseStudyNew.jsx
@@ -14,6 +14,7 @@ import PreloaderAnimationDark from "../../assets/PreloaderAnimationDark.json";
 
 const CaseStudyNew = () => {
 
+
     gsap.registerPlugin(useGSAP);
 
     const [t, i18next] = useTranslation();
